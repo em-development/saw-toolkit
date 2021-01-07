e30a6533a4c74d01fc72b5cbf2ec4d0e45eae0ed
Elys Marcos <dev@emdev.com.br>
2021-01-07 19:50:21 -0300

    Add new keys to enums

--- src/Enums/AppsEnum.php
+++ src/Enums/AppsEnum.php
@@ -8,4 +8,6 @@ abstract class AppsEnum
 {
     const ID = 1;
     const PROFILE = 2;
+    const DASHBOARD = 3;
+    const SCHEDULE = 4;
 }
--- src/Enums/TypeContactEnum.php
+++ src/Enums/TypeContactEnum.php
@@ -7,4 +7,6 @@ namespace SawToolkit\Enums;
 abstract class TypeContactEnum
 {
     const EMAIL_LOGIN = 1;
+    const EMAIL_NOTIFICATION = 2;
+    const PHONE = 3;
 }
